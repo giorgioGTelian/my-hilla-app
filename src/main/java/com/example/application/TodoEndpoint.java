a112ca11c05258d61965801815ed1c39b8f689ba
giorgio <giorgio.programmer@gmail.com>
2023-05-29 14:42:34 +0200

    fixing the bug

--- src/main/java/com/example/application/TodoEndpoint.java
+++ src/main/java/com/example/application/TodoEndpoint.java
@@ -5,13 +5,15 @@ import java.util.List;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 import dev.hilla.Nonnull;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Endpoint 
 @AnonymousAllowed 
 public class TodoEndpoint {
+
   private TodoRepository repository;
 
+  @Autowired // Ensure that Spring injects the repository
   public TodoEndpoint(TodoRepository repository) { 
     this.repository = repository;
   }
--- src/main/java/com/example/application/TodoRepository.java
+++ src/main/java/com/example/application/TodoRepository.java
@@ -1,6 +1,8 @@
 package com.example.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
 }
