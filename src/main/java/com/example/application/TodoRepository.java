4ae09fbb7606a11e84a08a36e011c64c8e496632
giorgio <giorgio.programmer@gmail.com>
2023-05-29 12:42:33 +0200

    trying to fix the issue

--- src/main/java/com/example/application/TodoRepository.java
+++ src/main/java/com/example/application/TodoRepository.java
@@ -2,8 +2,5 @@ package com.example.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-
-    Todo saveAll(Todo todo);
 }
